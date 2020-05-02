7f4589865eeb8ff72280579b6175b7980107c17f
fatima mabrouk <onlyfefe03@gmail.com>
2020-05-02 04:44:41 +0100

    node first app npm

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-var react = {
-    parmetire : (x,y) => (2*(x*y)),
-    area : (x,y) => (x*y)
-};
+// var react = {
+//     parmetire : (x,y) => (2*(x*y)),
+//     area : (x,y) => (x*y)
+// };
+const react = require('./rectangle')
 
+function rectangleArea(l, b) {
+    console.log('calculte the area of rectangle');
+    if(l <= 0 || b <= 0){
+        console.log(" the area of reactangle is Zero .......");
+    }
+    else {
+        console.log("the area of reactangle is " + react.area(l, b));
+        console.log("the parmiter of rectangle is " +  react.parmiter(l, b));
+    }
+}
 
-// function rectangleArea(l, b) {
-//     console.log('calculte the area of rectangle');
-//     if(l <= 0 || b <= 0){
-//         console.log(" the area of reactangle is Zero .......");
-//     }
-//     else {
-//         console.log("the area of reactangle is " + react.area(l, b));
-//         console.log("the parmiter of rectangle is " +  react.parmetire(l, b));
-//     }
-// }
-
-// rectangleArea(2, 4);
-// rectangleArea(2, 5);
-// rectangleArea(2, 0);
+rectangleArea(2, 4);
+rectangleArea(2, 5);
+rectangleArea(2, 0);
 console.log("welcome to first node app ........");
---
+++ rectangle.js
@@ -0,0 +1,2 @@
+exports.area = (x, y) => (x*y);
+exports.parmiter = (x, y) => (2*(x*y));
